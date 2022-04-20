5cb4d48f3923cc29b4af6d47be62d74d3618522a
Taher <ttaherhagui@gmail.com>
2022-04-20 18:21:30 +0100

    add update route.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   namespace 'api' do
     namespace 'v1' do
-      resources :tasks , only: [:index]
+      resources :tasks , only: [:index, :update]
     end
   end
 end
