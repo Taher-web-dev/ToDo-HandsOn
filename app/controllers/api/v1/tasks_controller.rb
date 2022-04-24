3c94bd325686f5f7f40fc4c11c2f536778ada645
Taher <ttaherhagui@gmail.com>
2022-04-24 17:55:08 +0100

    fix linter.

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -3,37 +3,40 @@ module Api
     class TasksController < ApplicationController
       def index
         @tasks = Task.all
-        render json: { status: 'SUCCESS', message: 'Loaded posts', data: @tasks}, status: :ok
+        render json: { status: 'SUCCESS', message: 'Loaded posts', data: @tasks }, status: :ok
       end
 
       def update
         @task = Task.find(params[:id])
         if @task.update(params_update)
-          render json: {status: 'SUCCESS', message:'Task is updated', data:@task}, status: :ok
+          render json: { status: 'SUCCESS', message: 'Task is updated', data: @task }, status: :ok
         else
-          render json: {status: 'Error', message: 'Something went wrong!', data:@task.errors}, status: :unprocessable_entity
+          render json: { status: 'Error', message: 'Something went wrong!', data: @task.errors },
+                 status: :unprocessable_entity
         end
       end
 
       def destroy
         @task = Task.find(params[:id])
         if @task.destroy
-          render json: {status: 'SUCCESS', message:'Task deleted successfully', data:@task}, status: :ok
+          render json: { status: 'SUCCESS', message: 'Task deleted successfully', data: @task }, status: :ok
         else
-          render json: {status: 'Error', message: 'Something went wrong!', data:@task}, status: :unprocessable_entity
+          render json: { status: 'Error', message: 'Something went wrong!', data: @task }, status: :unprocessable_entity
         end
       end
 
       def create
         @task = Task.new(params_create)
         if @task.save
-          render json: {status: 'SUCCESS', message:'Task created successfully', data:@task}, status: :ok
+          render json: { status: 'SUCCESS', message: 'Task created successfully', data: @task }, status: :ok
         else
-          render json: {status: 'Error', message:"Something went wrong, Task doesn't created!", data:@task}, status: :unprocessable_entity
+          render json: { status: 'Error', message: "Something went wrong, Task doesn't created!", data: @task },
+                 status: :unprocessable_entity
         end
       end
 
-      private 
+      private
+
       def params_update
         params.require(:task).permit(:description, :done)
       end
